c0a1f7b31606928784e9cb795f6718696599da1b
theoriaarhidoxa <88836579+theoriaarhidoxa@users.noreply.github.com>
2022-11-15 15:40:41 +0300

    Add files via upload

---
+++ src/components/OpenMeteo.tsx
@@ -0,0 +1,22 @@
+import React, {FC} from 'react';
+import {withFetch} from "../hoc/withFetch";
+import {ComponentProps, ICities} from "../interfaces";
+
+const OpenMeteo: FC<ComponentProps> = props => {
+    return (
+        <>
+            {props.data.map((el: ICities, i: number) => {
+                const {city, temperature} = el;
+                return (
+                    <tr key={city}>
+                        <td>{i + 1}.</td>
+                        <td>{city}</td>
+                        <td>{temperature} C&deg;</td>
+                    </tr>
+                )
+            })}
+        </>
+    );
+};
+
+export default withFetch(OpenMeteo, 'openMeteo');
